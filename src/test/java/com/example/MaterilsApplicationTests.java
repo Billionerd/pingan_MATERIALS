dead539ae6a612d87f505e1898bfe378f09d1075
Billionerd <xiehua@pingan.com>
2019-11-03 00:00:00 +0800

    pingan_MATERIALS

---
+++ src/test/java/com/example/MaterilsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+/**
+ *    中国平安保险（集团）股份有限公司
+ *    待上线域名pingan.com
+ *    http://39.103.176.186/  admin:pingan
+ *    xiehua@pingan.com
+ **/
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaterilsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
